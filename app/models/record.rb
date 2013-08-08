c29a4dcf29644bd8b165d189dd6c7c131f305754
Michael Slone <m.slone@gmail.com>
2013-08-08 09:21:36 -0400

    fix typo

--- app/models/record.rb
+++ app/models/record.rb
@@ -2,7 +2,7 @@ class Record < ActiveRecord::Base
   attr_accessible :abstract, :archival_container_format_id, :date, :file_format_id, :format_id, :kytopic_id,  :language_ids, :repository_id, :subject_id, :title, :type_id 
   belongs_to :format
   belongs_to :type
-  has_and_belongs_to_many :language
+  has_and_belongs_to_many :languages
   belongs_to :archival_container_format
   belongs_to :subject
   belongs_to :file_format
