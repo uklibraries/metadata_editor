ae523c6631c437e68eab4bd0c0dc562125109a43
Michael Slone <m.slone@gmail.com>
2013-08-14 08:33:51 -0400

    fix data type for rights field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130813170617) do
+ActiveRecord::Schema.define(:version => 20130814123152) do
 
   create_table "archival_container_formats", :force => true do |t|
     t.string   "name"
@@ -72,9 +72,9 @@ ActiveRecord::Schema.define(:version => 20130813170617) do
     t.integer  "kytopic_id"
     t.integer  "repository_id"
     t.integer  "type_id"
-    t.datetime "created_at",                   :null => false
-    t.datetime "updated_at",                   :null => false
-    t.string   "rights"
+    t.datetime "created_at",                                  :null => false
+    t.datetime "updated_at",                                  :null => false
+    t.text     "rights",                       :limit => 255
   end
 
   add_index "records", ["archival_container_format_id"], :name => "index_records_on_archival_container_format_id"
