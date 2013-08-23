2a2f79bbfb04b43651b2455fe8d6a5a726c2fbfa
Richard Maiti <rrmait0@zenodotus.uky.edu>
2013-08-23 14:35:03 -0400

    Add simple recommended fields

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,5 +1,5 @@
 class Record < ActiveRecord::Base
-  attr_accessible :creator, :date, :format_id, :kentucky_topic_id,  :language_ids, :repository_id, :source_id, :subject_ids, :title, :resource_type_id
+  attr_accessible :creator, :date, :description, :format_id, :kentucky_topic_id,  :language_ids, :publisher, :repository_id, :series_statement, :source_id, :subject_ids, :title, :resource_type_id
   belongs_to :format
   belongs_to :resource_type
   has_and_belongs_to_many :languages
