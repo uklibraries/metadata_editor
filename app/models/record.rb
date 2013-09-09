69d31aa973560d9181c53a3cff9ff9e04f9d90eb
Michael Slone <m.slone@gmail.com>
2013-09-09 16:15:24 -0400

    add retention schedule per #39

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,11 +1,12 @@
 class Record < ActiveRecord::Base
-  attr_accessible :creator, :date, :description, :format_id, :kentucky_topic_ids,  :language_ids, :publisher, :repository_id, :series_statement, :source_id, :subject_ids, :title, :resource_type_id
+  attr_accessible :creator, :date, :description, :format_id, :kentucky_topic_ids,  :language_ids, :publisher, :repository_id, :resource_type_id, :retention_date, :retention_id, :series_statement, :source_id, :subject_ids, :title
   belongs_to :format
   belongs_to :resource_type
   has_and_belongs_to_many :languages
   has_and_belongs_to_many :subjects
   has_and_belongs_to_many :kentucky_topics 
   belongs_to :repository
+  belongs_to :retention
   belongs_to :source
  
   validates :format_id,  :language_ids, :resource_type_id, :title, presence: true
