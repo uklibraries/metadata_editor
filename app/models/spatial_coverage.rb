defac162fb26cd02704ab9179b850bc4bfac3308
Richard Maiti <rrmait0@zenodotus.uky.edu>
2013-09-12 14:45:45 -0400

    updates to spatial coverage using find_or_create_by_name

--- app/models/spatial_coverage.rb
+++ app/models/spatial_coverage.rb
@@ -1,4 +1,9 @@
 class SpatialCoverage < ActiveRecord::Base
   attr_accessible :name
   has_and_belongs_to_many :records
+
+  validates :name, :uniqueness => true 
 end
+
+name = SpatialCoverage.find_or_create_by_name('38.031912, -84.495327')
+
